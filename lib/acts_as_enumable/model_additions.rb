2168bb9b332c13618976081daaed240b6afa6350
PeterWong <peter@primitus.com>
2012-01-27 02:59:08 +0800

    add comment for yard

--- lib/acts_as_enumable/model_additions.rb
+++ lib/acts_as_enumable/model_additions.rb
@@ -1,5 +1,53 @@
 module ActsAsEnumable
   module ModelAdditions
+    # To make the attribute an enum.
+    #
+    #   class User < ActiveRecord::Base
+    #     # assumes the column exists:
+    #     #   integer :role_enum
+    #
+    #     acts_as_enumable(:role, %w(admin staff helper member), defaul: "member")
+    #   end
+    #
+    # Available options are:
+    #
+    #   (optional) default: String / Symbol
+    #     default value, will be set to nil if it is not provided or it does not match the preset list
+    #     eg.
+    #     acts_as_enumable :role, %w(admin staff), default: :something_not_exist # set the default value to nil
+    #     acts_as_enumable :role, %w(admin staff), default: "staff" # set the defaut value to "staff"
+    #
+    # This enables the following methods:
+    #
+    #   user = User.first
+    #   User::ROLES
+    #   # ["admin", "staff", "helper", "member"]
+    #
+    #   User.roles_for_select("users.roles")
+    #   # [
+    #   #   { key: "admin", value: I18n.t("users.roles.admin") },
+    #   #   { key: "staff", value: I18n.t("users.roles.staff") }, ...
+    #   # ]
+    #
+    #   User.default_role
+    #   # "member"
+    #
+    #   User.default_role_enum
+    #   # 3
+    #
+    #   user.role
+    #   # "member"
+    #
+    #   user.role = "staff" # or user.role = :staff
+    #   user.role
+    #   # "staff"
+    #
+    #   user.role_enum # if user.role == "staff"
+    #   # 1
+    #
+    #   user.role_enum = 0
+    #   user.role
+    #   # "admin"
     def acts_as_enumable(attribute_name, enum_values, options={})
       const_set(attribute_name.to_s.pluralize.upcase, enum_values)
 
